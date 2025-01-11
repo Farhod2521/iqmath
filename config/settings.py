036d4a89ef14c0368659d5cb8888b39b0e7c1d77
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-01-11 21:01:28 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,6 @@
 
 import os
-from pathlib import Path
-FORCE_SCRIPT_NAME = '/python'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
