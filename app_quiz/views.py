1904e9655e0570abca65089fb63ca58a9a3b4647
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-01-19 20:57:57 +0500

    fixed

--- app_quiz/views.py
+++ app_quiz/views.py
@@ -145,7 +145,7 @@ class ResultListView(APIView):
         student = get_object_or_404(Student, id=student_id)
 
         # Eng oxirgi natijani olish
-        result = Result.objects.filter(student=student).first()
+        result = Result.objects.filter(student=student).last()
 
         if not result:
             return Response({"error": "No results found for this student"}, status=status.HTTP_404_NOT_FOUND)
