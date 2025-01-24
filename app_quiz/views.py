56a320affa685af5d958271f6d47b4141d82f71c
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-01-24 15:27:15 +0500

    fixed

--- app_quiz/views.py
+++ app_quiz/views.py
@@ -208,7 +208,7 @@ class QuizListView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+from datetime import timedelta
 
 class ResultCreateAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -302,7 +302,7 @@ class ResultCreateAPIView(APIView):
             "correct_questions": correct_questions,
             "incorrect_questions": incorrect_questions,
             "random_score": random_score,
-            "test_time": result.test_time,
+            "test_time": str(timedelta(seconds=result.test_time)),
         }
         return Response(response_data, status=status.HTTP_201_CREATED)
 
